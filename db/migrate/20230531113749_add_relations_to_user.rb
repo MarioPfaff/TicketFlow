874c2124fc14f1ac42f1b5787d39274c7ac8f1bf
lucavm203 <vanmalsenluca@gmail.com>
2023-05-31 16:07:35 +0200

    hier zo zijn de changes aan de foreign key

--- db/migrate/20230531113749_add_relations_to_user.rb
+++ db/migrate/20230531113749_add_relations_to_user.rb
@@ -1,7 +1,6 @@
 class AddRelationsToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :tickets do |t|
-      t.belongs_to :user, foreign_key: true
-    end
+      add_reference :tickets, :users, foreign_key:true
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_151423) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_113749) do
   create_table "tickets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_151423) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id"
+    t.index ["users_id"], name: "index_tickets_on_users_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_151423) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tickets", "users", column: "users_id"
 end
