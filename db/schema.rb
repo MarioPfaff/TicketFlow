8c28b4b18d3f52cce9b14c3bc53b02abcd007ba9
MarioPfaff <mariojuliaanpfaff@gmail.com>
2023-06-16 16:45:32 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -88,7 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_111743) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "tickets", "users"
+  add_foreign_key "tickets", "users", column: "users_id"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
