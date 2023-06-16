76603bb48e4ef210c9d2f01ccdc1f45240cd66f5
MarioPfaff <mariojuliaanpfaff@gmail.com>
2023-06-16 23:01:18 +0200

    Add .gitignore for database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_13_111743) do
-  create_table "tickets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.string "label", limit: 20, default: "Niet gecategoriseerd"
-    t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.bigint "users_id"
-    t.integer "priority", default: 0
-    t.index ["user_id"], name: "index_tickets_on_user_id"
-    t.index ["users_id"], name: "index_tickets_on_users_id"
-  end
-  
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -51,6 +37,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_111743) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "tickets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "label", limit: 20, default: "Niet gecategoriseerd"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "users_id"
+    t.integer "priority", default: 0
+    t.index ["user_id"], name: "index_tickets_on_user_id"
+    t.index ["users_id"], name: "index_tickets_on_users_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -88,7 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_111743) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "tickets", "users", column: "users_id"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "tickets", "users", column: "users_id"
 end
